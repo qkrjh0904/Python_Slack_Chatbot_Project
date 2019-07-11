3053d58e1b5c379c73d4b8e61064ede647f7113d
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-07-11 15:57:29 +0900

    [추가] 해먹남녀 해먹지수 크롤링

--- index.py
+++ index.py
@@ -52,10 +52,15 @@ def _crawl_music_chart(text):
     soup = BeautifulSoup(sourcecode, "html.parser")
 
     keywords = []
+    score = soup.find_all("span", class_="judge")
     time = soup.find_all("div", class_="time")
     btn_like = soup.find_all("button", class_="btn_like")
     for i in range(12):
-        keywords.append(str(i+1)+"위 - 조리시간 : " + time[i].get_text().strip() + " / 좋아요 수 : " + btn_like[i].get_text().strip())
+        keywords.append(str(i+1)+"위 - " 
+        + " / 해먹지수 : " + score[i].find("strong").get_text().strip()
+        + " / 조리시간 : " + time[i].get_text().strip() 
+        + " / 좋아요 수 : " + btn_like[i].get_text().strip()
+        )
 
     # 한글 지원을 위해 앞에 unicode u를 붙혀준다.
     return u'\n'.join(keywords)
